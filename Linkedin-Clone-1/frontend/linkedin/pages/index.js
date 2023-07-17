0aa57c59641e2378c0a9f1b81b7e0b8e55d1fa67
KamruzzamanAsif <asif720kamruzzaman@gmail.com>
2023-07-17 20:31:37 +0600

    tailwind css setup

--- Linkedin-Clone-1/frontend/linkedin/pages/index.js
+++ Linkedin-Clone-1/frontend/linkedin/pages/index.js
@@ -3,7 +3,9 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <>
-      <p>My first page</p>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
 
       <h1 className="title">
         Read <Link href="/home">this page!</Link>
