776d56234cfac32a06987a2e50170d35b30fb74d
KamruzzamanAsif <asif720kamruzzaman@gmail.com>
2023-07-22 12:39:54 +0600

    signin connection not done

--- Linkedin-Clone-1/frontend/linkedin/pages/Signin.jsx
+++ Linkedin-Clone-1/frontend/linkedin/pages/Signin.jsx
@@ -1,10 +1,40 @@
 import Image from 'next/image';
 import logo from 'public/linkedin-logo.jpeg';
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const Signin = () => {
-    return(
-    <div className='bg-gray-200 md:container h-screen w-screen'>
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleInputChange = (event) => {
+    const { id, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [id]: value,
+    }));
+  };
+
+  const handleSignin = async () => {
+    try {
+      // Make a POST request to your backend API
+      const response = await axios.post('http://localhost:5000/signin', formData);
+
+      // Handle the response as needed (e.g., show success message or redirect to home page)
+      console.log('Signin successful!', response.data);
+
+      // Redirect to the home page (you should replace '/home' with the actual URL of your home page)
+      window.location.href = '/home';
+    } catch (error) {
+      // Handle errors (e.g., display error message)
+      console.error('Signin failed:', error);
+    }
+  };
+
+  return (
+    <div className="bg-gray-200 md:container h-screen w-screen">
       <div className="container mx-auto">
         <Image src={logo} alt="linkedin-logo" height={500} width={150} />
       </div>
@@ -15,14 +45,15 @@ const Signin = () => {
         </div>
 
         <div className="mb-6">
-          <label className="block text-gray-700 font-bold mb-2" htmlFor="username">
+          <label className="block text-gray-700 font-bold mb-2" htmlFor="email">
             Email address
           </label>
           <input
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="username"
+            id="email"
             type="email"
             placeholder="Email address"
+            onChange={handleInputChange}
           />
         </div>
 
@@ -35,6 +66,7 @@ const Signin = () => {
             id="password"
             type="password"
             placeholder="Password"
+            onChange={handleInputChange}
           />
         </div>
 
@@ -42,6 +74,7 @@ const Signin = () => {
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             type="button"
+            onClick={handleSignin} // Call handleSignin when the "Sign In" button is clicked
           >
             Sign In
           </button>
@@ -63,7 +96,7 @@ const Signin = () => {
         </div>
       </div>
     </div>
-    );
-}
+  );
+};
 
 export default Signin;
