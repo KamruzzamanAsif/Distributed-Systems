faabf392bfbdcface9ab1ce0df655a4bb01c6669
KamruzzamanAsif <asif720kamruzzaman@gmail.com>
2023-09-22 09:10:05 +0600

    notification service| all service works

--- Microservices/notification-service/app.js
+++ Microservices/notification-service/app.js
@@ -10,13 +10,13 @@ app.use("/", router);
 
 mongoose
     .connect(
-        "mongodb://127.0.0.1:27017/linkedin",
+        "mongodb://127.0.0.1:27017/notification_db",
         {useNewUrlParser: true, useUnifiedTopology: true}
     )
     .then( () => 
-        app.listen(5000, () => console.log("Connected to MongoDB and listening on port 5000"))
+        app.listen(5003, () => console.log("Connected to MongoDB notification_db and listening on port 5003"))
     )
-    .catch(err => console.log("Error connecting to MongoDB" + err));
+    .catch(err => console.log("Error connecting to MongoDB notification_db" + err));
 
 
 // Start the notification cleaner job
